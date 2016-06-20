5d62330b0e095fe180f872528345286397ff152f
JacopoMartelli <martelli.jacopo@gmail.com>
2016-06-20 10:01:25 +0000

    Add files via upload

--- share_belen.py
+++ share_belen.py
@@ -16,9 +16,16 @@ import h5py
 #==============================================================================
 class Connection:
     def __init__(self,host):
+        ''' 
+        Put the host of the database.         
+        '''
         self.host=host
         
-    def find_by_var(self,link='',var='',dat = True):
+    def _find_by_var(self,link='',var='',dat = True):
+        ''' 
+        Return the link or the value of the var entry in the database.
+        If dat=True: return link else dat=False return value of var         
+        '''
         #dat=True return link, dat=False return id
         l=[]
         r = requests.get(os.path.join(self.host+'/anechodb/api/v1/'+link))
@@ -37,41 +44,86 @@ class Connection:
                     if var in s[i]['links'][j]['rel']:
                          l.append(s[i]['data'][0]['value'])
         return l
-    def read_link(self,link='',idl=''):
-        r = requests.get(os.path.join(self.host+'/anechodb/api/v1/'+link+'/'+idl))
+    def read_link(self,link,idl):
+        ''' 
+        Print the json collection of the chosen link entry.
+        Input:
+            link(string):the link to the page of the database. It can be only:
+                         'operators','instruments','projects','measurements','beams'.
+            idl(int): identifier of the page of the link .        
+        '''
+        r = requests.get(os.path.join(self.host+'/anechodb/api/v1/'+link+
+                        '/%d'%idl))
         if r.status_code==200:
             print(json.loads(r.text))
     
-    def search_meas_by_instrument(var=''):
+    def search_meas_by_instrument(self,var=''):
+        ''' 
+        Search wich measurements are linked at the instrument decided by var entry.
+        Input:
+            var(string):The instrument used for the search (example 'VNA').
+        Output:
+            m_id(array of int):The identifier of the measurement that use the 
+                               instrument.
+        '''
+
         if var:
             m_id=[]
-            idl=Connection.find_by_var('instruments',var,True)
+            idl=Connection._find_by_var(self,'instruments',var,True)
             if idl:
                 idl=idl.split('/')[-1]
                 rel="/api/v1/instruments/"+str(idl)
-                m_id=Connection.find_by_var('measurements',rel,False)
+                m_id=Connection._find_by_var(self,'measurements',rel,False)
             return m_id
         
-    def search_meas_by_projects(var=''):
+    def search_meas_by_projects(self,var=''):
+        ''' 
+        Search wich measurements are linked at the project decided by var entry.
+        Input:
+            var(string):The project used for the search (example 'LSPE').
+        Output:
+            m_id(array of int):The identifier of the measurement that use the 
+                               project.
+        '''
+
         if var:
             m_id=[]
-            idl=Connection.find_by_var('projects',var,True)
+            idl=Connection._find_by_var(self,'projects',var,True)
             if idl:
                 idl=idl.split('/')[-1]
                 rel="/api/v1/projects/"+str(idl)
-                m_id=Connection.find_by_var('measurements',rel,False)
+                m_id=Connection._find_by_var(self,'measurements',rel,False)
             return m_id
-    def search_beam_by_meas (m_id=0):
+    def search_beam_by_meas (self,m_id=0):
+        ''' 
+        Search wich beams are linked at the measurement identifier decided 
+        by m_id entry.
+        Input:
+            m_id(int):The measurement identifier used for the search (example 1).
+        Output:
+            b_id(array of int):The identifier of the beams linked at the
+                               chosen measurement.
+        '''        
+        
         b_id=[]
         if m_id:
             rel="/api/v1/measurements/"+str(m_id)
-            b_id=(Connection.find_by_var('beams',rel,False))
+            b_id=(Connection._find_by_var(self,'beams',rel,False))
         return b_id
     def get_beam_in_dict_by_id(self,b_id):
+        ''' 
+        Download the beam chosen by identifier as a dict variable.
+        Input:
+            b_id(int):The beam identifier to download (example 1).
+        Output:
+            beam(dict):The beam downloaded. It has 4 fields as the original .h5
+                       file plus the attribute field with some extra information.
+        '''
         beam={}
         #Connect and dowload the chosen beam
         head = {'Accept': 'application/x-hdf5'}
-        r=requests.get(os.path.join(self.host+'/anechodb/api/v1/beams/%s'%b_id),headers=head)
+        r=requests.get(os.path.join(self.host+'/anechodb/api/v1/beams/%d'%b_id),
+                       headers=head)
         f_b, p_b = tempfile.mkstemp(suffix='.h5')
         try:
             with os.fdopen(f_b, 'wb') as tmp:
@@ -101,21 +153,56 @@ class Connection:
 #==============================================================================
 ### Computation
 #==============================================================================
-class computation:
+class Computation:
 
     def make_beam_meanvar(beam,f=[],start=0,stop=-1):
+        ''' 
+        Apply mean and variance at the data stored in beam at the chosen frequencies.
+        Input:
+            beam(dict):The beam to be computed
+            f(array of float):The frequencies of the measure to be computed.
+                              If empty all the frequencies in beam will be used.
+            start(int):Starting index of the measurement array for the computation.
+            stop(int):Stopping index of the measurement array for the computation.
+                                            
+        Output:
+            b(dict):The input beam with measurement changed with the mean and with
+                    a new field called Amplitude_Variance with the variance.
+        '''
         b=copy.deepcopy(beam)
         if not f:
             f=b['Frequencies']
         for i in range (len(f)):
             id_f=np.where(b['Frequencies']==f[i])
             if b['DUT']['F_%d'%id_f[0]]['Amplitude'].ndim>1:
-                b['DUT']['F_%d'%id_f[0]]['Amplitude_Variance']=np.var(b['DUT']['F_%d'%id_f[0]]['Amplitude'][:,start:stop],axis=1)    
-                b['DUT']['F_%d'%id_f[0]]['Amplitude']=np.mean(b['DUT']['F_%d'%id_f[0]]['Amplitude'][:,start:stop],axis=1)
+                b['DUT']['F_%d'%id_f[0]]['Amplitude_Variance']=np.var(b['DUT']
+                            ['F_%d'%id_f[0]]['Amplitude'][:,start:stop],axis=1)    
+                b['DUT']['F_%d'%id_f[0]]['Amplitude']=np.mean(b['DUT']
+                            ['F_%d'%id_f[0]]['Amplitude'][:,start:stop],axis=1)
         return b
         
         
     def center_norm_beam(beam,f=[], center = True, norm = True):
+        ''' 
+        Apply normalization and centering at the data stored in beam.
+        Input:
+            beam(dict):The beam to be computed. If Amplitude in beam is a matrix,
+                        the mean of the matrix will be used for this computation.
+            f(array of float):The frequencies of the measure to be computed.
+                              If empty all the frequencies in beam will be used.
+            center(bool or int/float):If center=True, apply centering. If it's 
+                                      a number, this will be used to correct the 
+                                      position.
+            norm(bool or int/float):If norm=True, apply normalization. If it's 
+                                      a number, this will be used as normalization
+                                      factor.
+                                            
+        Output:
+            b(dict):The input beam with Amplitude and Positions computed. The
+                    positions of the original beam are stored in Original_Positions
+                    field and a new field called Correction is created with 
+                    centering and normalization factors stored for each frequency.
+        '''
         b=copy.deepcopy(beam)  
         corr={}
         P={}     
